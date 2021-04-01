e300be869833f549f1d5142bca26759cf79ff065
fred <fred@163.com>
2021-04-01 16:45:31 +0800

    第一次提交

--- src/main/java/com/turing/demo/DemoApplication.java
+++ src/main/java/com/turing/demo/DemoApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     public static void main(String[] args) {
+
+        System.out.println("第一次提交");
+
         SpringApplication.run(DemoApplication.class, args);
     }
 
